e3dfff6731cd1ae74174bc35e9863c199955f280
astern <alena7690@gmail.com>
2019-03-13 11:27:40 -0500

    pushing outline

---
+++ src/analysis/data_analysis_outline.R
@@ -0,0 +1,44 @@
+### Step 1: Split Data into Training, Validation, Testing Sets ###
+
+#Q: do we have enough data to do this?
+
+### Step 2: Define All Variables to Use in Model and Appropriate Model ###
+
+#Q: I think there are standard statistical tests that help you pick the right model (eg Poison, linear, etc.)
+# Use training data to identify good model 
+
+### Step 3: Use Lasso Regression, Ridge Regression, Elastic Net, and Forward Selection to Identify Subset of Variables ###
+
+#Q: we'd have to rewrite code for forward selection
+
+### Step 3a:Lasso
+
+### Step 3b: Ridge
+
+### Step 3c: Elastic Net
+
+### Step 3d: Forward Selection
+
+
+### Step 4: Assess Model on Test Set ###
+
+### Step 4a: Assess accuracy in terms of minimizing RMSE
+
+### Step 4b: Calculate 90% quantile
+
+### Step 4c: Calculate prediction interval and discuss width
+
+### Step 4d: Assess coverage of prediction interval on test set
+
+
+### Step 5: Local Hypothesis Testing ###
+
+#Q: Not splitting data
+### 5a: fit final model on each local hypothesis (each block group = one hypothesis)
+
+### 5b: use false discovery 
+
+
+
+
+
